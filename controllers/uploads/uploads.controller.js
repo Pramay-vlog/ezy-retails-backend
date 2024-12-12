9f7eb2a8723d62cde617ec1e66d58b17dfd5ebb8
shivammakrubiya <shivampatel41015@gmail.com>
2024-12-12 16:21:45 +0530

    Fix uploadFiles method to handle multiple file uploads

--- controllers/uploads/uploads.controller.js
+++ controllers/uploads/uploads.controller.js
@@ -6,12 +6,13 @@ module.exports = exports = {
 
     /* Create Upload API */
     uploadFiles: async (req, res) => {
-        if (!req.file) return response.INTERNAL_SERVER_ERROR({ res, message: messages.INTERNAL_SERVER_ERROR })
+        if (req.files.length === 0) return response.INTERNAL_SERVER_ERROR({ res, message: messages.INTERNAL_SERVER_ERROR })
         return response.OK({
             res, payload: {
                 path:
-                    req?.file?.key.split("/").slice(0, req?.file?.key.split("/").length - 1).join("/"),
-                url: req?.file?.location
+                    req?.files && req?.files.length > 0 && req?.files.map((file) => file.key.split("/").slice(0, file.key.split("/").length - 1).join("/")),
+                    // ?.key.split("/").slice(0, req?.file?.key.split("/").length - 1).join("/"),
+                url: req?.files && req?.files.length > 0 && req?.files.map((file) => file.location),
             }
         });
     },
